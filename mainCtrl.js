92e93fa2d2ac484857c015a42803cd8c9dcf1cf1
AShestakov <har.Storm@mail.ru>
2015-08-27 18:30:03 +0300

    add editable func

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,15 +1,55 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', ['$scope', function ($scope) {
-$scope.persons = [];
-$scope.id = 0;
+	$scope.persons = [{
+      id: 1,
+      name: 'vasya',
+      mobile: '1234134',
+      adress: 'tratata'
+    }, {
+      id: 2,
+      name: 'petya',
+      mobile: '1234231134',
+      adress: 'tratdsdcata'
+    }, {
+      id: 3,
+      name: 'her',
+      mobile: '1896634',
+      adress: 'ddddddd'
+    }];
 
-$scope.addRow = function(id){
-	$scope.id++;
-    $scope.persons.push({name:'', mobile:'', adress:''});
-}
+	$scope.addRow = function(){
+		
+	    $scope.persons.push({name:'', mobile:'', adress:''});
+	}
 
-$scope.removeRow = function(index){
-    $scope.persons.splice(index,1);
-}
+	$scope.removeRow = function(index){
+	    $scope.persons.splice(index,1);
+	}
+
 }]);
+
+app.directive( 'editInPlace', function() {
+  return {
+    restrict: 'E',
+    scope: { value: '=' },
+    template: '<span ng-click="edit()" ng-bind="value"></span><input ng-model="value"></input>',
+    link: function ( $scope, element, attrs ) {
+
+      var inputElement = angular.element( element.children()[1] );
+      element.addClass( 'edit-in-place' );
+      $scope.editing = false;
+
+      $scope.edit = function () {
+        $scope.editing = true;
+        element.addClass( 'active' );
+        inputElement[0].focus();
+      };
+
+      inputElement.prop( 'onblur', function() {
+        $scope.editing = false;
+        element.removeClass( 'active' );
+      });
+    }
+  };
+});
